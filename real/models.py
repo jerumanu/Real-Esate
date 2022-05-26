f6b46cd6f365262780e7786581f52576173c9442
jerumanu <emmanueljeru200@gmail.com>
2022-05-26 12:26:25 +0300

    created and update detai post estaet models

--- real/models.py
+++ real/models.py
@@ -1,20 +1,36 @@
 from django.db import models
-from django.forms import CharField, ImageField, IntegerField
+
+
 
 
 
 
 class Detail(models.Model):
-    title=CharField(max_length=50)
-    detail_descrip=CharField(max_length=50)
-    image= ImageField()
-    bedRooms= IntegerField()
-    bathRoom=ImageField()
+    # title=CharField(max_length=50)
+    detail_descripe=models.CharField(max_length=50)
+    photos= models.ImageField()
+    bedRooms=  models.IntegerField()
+    bathRoom=models.ImageField()
 
 
     def __str__(self):
         return self.title
 
-class post(models.Model):
-    name=CharField(max_length=50)
-    phone_no= IntegerField()
+class Post(models.Model):
+    name=models.CharField(max_length=50)
+    phone_no= models.IntegerField()
+    email=models.EmailField()
+
+
+class Estate(models.Model):
+    locations= models.CharField(max_length=20)
+    price=models.IntegerField()
+    image=models.ImageField()
+
+class Amentitis(models.Model):
+    security=models.CharField(max_length=10)
+    parking=models.CharField(max_length=10) 
+    power=models.CharField(max_length=10)
+    pool=models.CharField(max_length=10)
+    water=models.CharField(max_length=10) 
+
