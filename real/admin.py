83a7abbacc3ba5ce7b5dade0a08715ac15f6a843
jerumanu <emmanueljeru200@gmail.com>
2022-05-26 12:25:50 +0300

    register models

--- real/admin.py
+++ real/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Detail)
+admin.site.register(Post)
+admin.site.register(Estate)
+admin.site.register(Amentitis)
+
