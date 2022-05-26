7e2007fe0be805e8d8a5a5a3fc99834bfeec606b
jerumanu <emmanueljeru200@gmail.com>
2022-05-26 11:03:01 +0300

    installtions

--- real/models.py
+++ real/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.forms import CharField, ImageField, IntegerField
 
 
+
+
+class Detail(models.Model):
+    title=CharField(max_length=50)
+    detail_descrip=CharField(max_length=50)
+    image= ImageField()
+    bedRooms= IntegerField()
+    bathRoom=ImageField()
+
+
+    def __str__(self):
+        return self.title
+
+class post(models.Model):
+    name=CharField(max_length=50)
+    phone_no= IntegerField()
