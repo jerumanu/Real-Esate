9820398559d2acb2c2f0f041cf01a79d2a81092c
jerumanu <emmanueljeru200@gmail.com>
2022-05-26 12:27:45 +0300

    craeted  views using view sets

--- real/views.py
+++ real/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import viewsets # new
 
+
+
+from .models import *
+from .serializers import *
 # Create your views here.
+
+
+class DetailViewSet(viewsets.ModelViewSet):  # new
+
+
+   
+    queryset = Detail.objects.all()
+    serializer_class = DetailSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):  # new
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class EstateViewSet(viewsets.ModelViewSet):  # new
+
+    queryset = Estate.objects.all()
+    serializer_class = EstateSerializer
+
+
+class AmentitisViewSet(viewsets.ModelViewSet):  # new
+
+    queryset =Amentitis.objects.all()
+    serializer_class =AmentitisSerializer
