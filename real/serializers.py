20de1c77d55d2f83c08d42d8955416d9b9d33a48
jerumanu <emmanueljeru200@gmail.com>
2022-05-26 12:26:38 +0300

    serialized models

---
+++ real/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import *
+
+
+class DetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Detail
+        fields = ['id', 'title', 'detail_descripe', 'photos', 'bedRooms', 'bathRooms']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'name', 'phone_no','email']
+
+
+class EstateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estate
+        fields = ['id', 'location', 'price',  'image', 'description']
+
+
+class AmentitisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Amentitis
+        fields = ['id', 'security', 'parking','power', 'pool', 'water']
+
+
+
+
